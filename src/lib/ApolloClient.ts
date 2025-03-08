724d74b2f50e7ca92e95ef953eda5c42c4e8e42e
abdul-wajid270 <abdulwajid2818@gmail.com>
2025-03-09 00:44:43 +0500

    Added production URL

--- src/lib/ApolloClient.ts
+++ src/lib/ApolloClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const API_URL = process.env.NEXT_PUBLIC_API_GRAPHQL_URL || 'http://localhost:3000/api/v1/graphql';
+const API_URL = process.env.NEXT_PUBLIC_API_GRAPHQL_URL || 'https://full.kitchen/api/v1/graphql';
 
 export const client = new ApolloClient({
   uri: API_URL,
