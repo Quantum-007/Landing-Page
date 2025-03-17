89cb1a64210017fc7eaa43599c4ab99b389098f7
abdul-wajid270 <abdulwajid2818@gmail.com>
2025-03-17 14:59:11 +0500

    Fixed checkboxes issue

--- src/components/home/atoms/JoinPilotProgram.tsx
+++ src/components/home/atoms/JoinPilotProgram.tsx
@@ -1,73 +1,48 @@
 import React, { useState } from 'react';
 import JoinPilotProgramAsBusinessForm from './JoinPilotProgramAsBusinessForm';
-import JoinPilotProgramAsCustomer from './JoinPilotProgramAsCustomer'
-import { Box, Typography, Checkbox, FormControlLabel, Button, Grid } from '@mui/material';
+import JoinPilotProgramAsCustomer from './JoinPilotProgramAsCustomer';
+import { Box, Typography, Button } from '@mui/material';
 
 const JoinPilotProgram = () => {
   const [mainFormVisible, setMainFormVisible] = useState(true);
-
   const [selectedForm, setSelectedForm] = useState<'customer' | 'business' | null>('customer');
 
   const handleSelection = (type: 'customer' | 'business') => {
     setSelectedForm(type);
-  };
+    setMainFormVisible(false);
+  }
 
   return (
     <Box display="flex" flexDirection="column" alignItems="start" gap={4} p={3}>
       {mainFormVisible && (
         <>
-          <Typography variant="h5" sx={{ color: 'white', font: 'bold' }}>
+          <Typography variant="h5" sx={{ color: 'white', fontWeight: 'bold' }}>
             Choose How You Would Like to Sign Up:
           </Typography>
-          <Box className='flex flex-row mt-4 gap-10 w-full'>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={selectedForm === 'customer'}
-                  onChange={() => handleSelection('customer')}
-                  sx={{
-                    color: 'rgba(255, 255, 255, 0.7)',
-                    '&.Mui-checked': { color: '#3c5a1e' },
-                  }}
-                />
-              }
-              label="Customer"
-              sx={{ color: 'white' }}
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={selectedForm === 'business'}
-                  onChange={() => handleSelection('business')}
-                  sx={{
-                    color: 'rgba(255, 255, 255, 0.7)',
-                    '&.Mui-checked': { color: '#3c5a1e' },
-                  }}
-                />
-              }
-              label="Business"
-              sx={{ color: 'white' }}
-            />
-          </Box>
-
-          <Grid item xs={12} width="100%" className='mt-10'>
+          <Box className='flex flex-row gap-2 mt-4 w-full'>
             <Button
-              variant="contained"
-              fullWidth
-              disabled={!selectedForm}
-              onClick={() => setMainFormVisible(false)}
+              variant='contained'
+              onClick={() => handleSelection('customer')}
               sx={{
-                backgroundColor: '#3c5a1e',
-                color: 'white',
-                textTransform: 'none',
-                '&:hover': {
-                  backgroundColor: '#2e4717',
-                },
+                color: selectedForm === 'customer' ? 'white' : '#808080',
+                backgroundColor: selectedForm === 'customer' ? '#3c5a1e' : 'transparent',
+                paddingX: 4
               }}
             >
-              Next
+              Customer
             </Button>
-          </Grid>
+            <Button
+              variant='contained'
+              onClick={() => handleSelection('business')}
+              sx={{
+                color: selectedForm === 'business' ? 'white' : '#808080',
+                backgroundColor: selectedForm === 'business' ? '#3c5a1e' : 'transparent',
+                paddingX: 4
+              }}
+            >
+              Business
+            </Button>
+          </Box>
         </>
       )}
 
