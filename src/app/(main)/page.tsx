68a89bb187e8489700ab3ec989e2b6cb5b60033c
Whynotus777 <abdulmanan0715@gmail.com>
2025-08-21 17:02:38 -0400

    Update page.tsx

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,4 @@
-'use client';
+export default function HomePage() {
+  return <div><h1>It works!</h1></div>;
+}
 
-import Home from '@/components/home/Home';
-
-import { Box } from '@mui/material';
-
-const App = () => {
-  return (
-    <Box className="bg-gray-900 text-white min-h-screen">
-      <Box component="main">
-        <Home />
-      </Box>
-    </Box>
-  );
-};
-
-export default App;
